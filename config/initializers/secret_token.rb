24f587cf2b98a0ba3210cda4b89d7af944534c5a
anrake <anrake@gmail.com>
2014-01-12 12:55:24 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jcastle2::Application.config.secret_key_base = '1fc26f66ee38e8a5bde0d25db2cc2c1be9f2390906628b1046dd6db1d355e78bcae180039df6cc53a4abedfd58607e7b6f12cb70a449b4ff5e753b3c75c6d677'
