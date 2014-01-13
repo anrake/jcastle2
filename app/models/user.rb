2f20985d403d0e89424b39ecd22c08e3b1b32796
anrake <anrake@gmail.com>
2014-01-13 12:44:53 +0900

    minor updates for for foundation from bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	before_create :assign_role
+
+  	def assign_role
+    	# assign a default role if no role is assigned
+    	self.add_role :user if self.roles.first.nil?
+  	end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,14 @@ Jcastle2::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -25,11 +31,7 @@ Jcastle2::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode.
-  config.action_mailer.perform_deliveries = true
+  config.assets.debug = true  
+
 
 end
